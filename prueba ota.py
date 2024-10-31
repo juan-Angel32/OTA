8b3027cb040a115f4ee1a608906b922e39f4c6b6
Juan Angel Becerril Leal <becerrillealjuanangel@gmail.com>
2024-10-31 10:43:22 -0600

    Update prueba ota.py

--- prueba ota.py
+++ prueba ota.py
@@ -2,7 +2,7 @@ from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD  # Asegúrate de que el archivo tenga estas variables correctamente definidas.
 
 # URL del repositorio de firmware
-firmware_url = "https://github.com/juan-Angel32/prueba-ota"
+firmware_url = "main.py"
 
 # Configuración de la actualización OTA
 ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url)
